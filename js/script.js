f665fd6d8bf3771fe617da68fc47e530e67f7129
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-06-26 12:42:05 +0200

    Find the correct article using the selector

--- js/script.js
+++ js/script.js
@@ -15,8 +15,6 @@ function titleClickHandler(event){
 
   /* [DONE] add class 'active' to the clicked link */ 
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
@@ -29,7 +27,9 @@ function titleClickHandler(event){
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
   /* add class 'active' to the correct article */
 }
