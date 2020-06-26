e927002ef58bd46b424e21ca0e297ed48dcd0dc0
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-06-26 16:06:31 +0200

    add code indentation

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@ function titleClickHandler(event){
     console.log('Link was clicked!');
     console.log(event);
 
-  /* [DONE] remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+        activeLink.classList.remove('active');
     }
 
 
-  /* [DONE] add class 'active' to the clicked link */ 
+    /* [DONE] add class 'active' to the clicked link */ 
     clickedElement.classList.add('active');
     console.log(clickedElement);
 
-  /* [DONE] remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post');
 
     for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+        activeArticle.classList.remove('active');
     }
 
-  /* [DONE] get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
 
-  /* [DONE] add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
     console.log(targetArticle);
 }
@@ -41,5 +41,5 @@ function titleClickHandler(event){
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
